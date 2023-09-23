875c641047433601549757920870ee90150d9e99
solidity12 <volibear1001@gmail.com>
2023-09-23 22:58:19 +0900

    feat: Add test script

---
+++ test/GenesisNFT.test.js
@@ -0,0 +1,29 @@
+const { expect } = require("chai");
+
+describe("GenesisNFT contract", function () {
+  let genesisNFT;
+  let user;
+
+  beforeEach(async function () {
+    const GenesisNFT = await ethers.getContractFactory("GenesisNFT");
+    [user] = await ethers.getSigners();
+
+    genesisNFT = await GenesisNFT.deploy();
+    await genesisNFT.deployed();
+  });
+
+  describe("Minting", function () {
+    it("Should mint a token to an address", async function () {
+      await genesisNFT.connect(user).mintGenesisNFT();
+      expect(await genesisNFT.ownerOf(1)).to.equal(user.address);
+    });
+
+    it("Should prevent multiple minting from the same address", async function () {
+      await genesisNFT.connect(user).mintGenesisNFT();
+
+      await expect(
+        genesisNFT.connect(user).mintGenesisNFT()
+      ).to.be.revertedWith("You have already minted a token");
+    });
+  });
+});
