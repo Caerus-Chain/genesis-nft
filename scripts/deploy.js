b39cb719d35d6f3bfb028e4ad3d24f46d88197fa
solidity12 <volibear1001@gmail.com>
2023-09-23 23:00:53 +0900

    feat: Add deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,19 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Deploy the contract
+  const GenesisNFT = await hre.ethers.getContractFactory("GenesisNFT");
+  const genesisNFT = await GenesisNFT.deploy();
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  await genesisNFT.deployed();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("GenesisNFT deployed to:", genesisNFT.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// If the script fails, the promise will reject, causing the process to exit
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
